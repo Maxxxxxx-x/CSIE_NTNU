4759fa71f9d165996dbfbbffc38066d31229bc53
= <=>
2022-10-06 15:14:02 +0800

    Redid HW0101, Finished HW0102

--- Year1/Homework/HW1/hw0102.c
+++ Year1/Homework/HW1/hw0102.c
@@ -1,100 +1,111 @@
-#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
 
 char Separator[] = "--------\n";
 
-int GetErrorCode(){
-    srand(time(0));
-    for (int i = 0; i < 1; i++){
-        int num = (rand() % (499 - 400 + 1)) + 400;
-        return num;
+int GetDigit(int Num, int pos){
+ 
+    switch(pos){
+        case 1:
+        printf("%d", (Num / 100) % 10);
+        return (Num / 100) % 10;
+        
+        case 2:
+        printf("%d", (Num / 10) % 10);
+        return (Num / 10) % 10;
+        
+        case 3:
+        printf("%d", Num % 10);
+        return Num % 10;
     }
 }
 
-void GetDigits(int Num, int Type, int IsAns) {
-    int arr[100];
-    int i = 0;
-    int tmp1;
-    if (IsAns == 0){
-        printf("%s", Separator);
+int FormatPrint(int First, int Second, int Result){
+    int AType;
+    if (Result < 0){
+        AType = -1;
+        Result *= -1;
     }
-    switch(Type){
-        case -1:
-            Num = -Num;
-            printf(" - ");
-            break;
-        case 0:
-            printf("   ");
-            break;
-        case 1:
-            printf("-) ");
-            break;
-        default:
-            break;
-    }
-
-    while (Num != 0) {
-        tmp1 = Num % 10;
-        arr[i] = tmp1;
-        i += 1;
-        Num /= 10;
-    };
-    switch(i){
-        case 1:
-            printf("    ");
-            break;
-        case 2:
-            printf("  ");
-    }
-    for (int tmp2 = i - 1; tmp2 > -1; tmp2--){
-        printf("%d ", arr[tmp2]);
+    int F1, F2, F3;
+    int S1, S2, S3;
+    int R1, R2, R3;
+    F1 = GetDigit(First, 1);
+    F2 = GetDigit(First, 2);
+    F3 = GetDigit(First, 3);
+    printf("\n");
+    S1 = GetDigit(Second, 1);
+    S2 = GetDigit(Second, 2);
+    S3 = GetDigit(Second, 3);
+    printf("\n");
+    if (Result >= 100){
+        R1 = GetDigit(Result, 1);
+        R2 = GetDigit(Result, 2);
+        R3 = GetDigit(Result, 3);
+    }else if (Result >= 10 && Result <= 100){
+        R2 = GetDigit(Result, 2);
+        R3 = GetDigit(Result, 3);
+    } else if (Result > 0 && Result < 10){
+        R3 = Result;
     }
     printf("\n");
-}
 
-void Calculate(int Num1, int Num2){
-    int Ans = Num1 - Num2;
-    int AType = 0;
-    if (Ans < 0){
-        AType = -1;
+    printf("   %d %d %d\n", F1, F2, F3);
+    printf("-) %d %d %d\n", S1, S2, S3);
+    printf("%s", Separator);
+    if (AType == -1){
+        if (Result >= 100){
+            printf(" - %d %d %d\n", R1, R2, R3);
+            return 0;
+        }
+        if (Result >= 10 && Result < 100){
+            printf(" -   %d %d\n", R2, R3);
+            return 0;
+        }
+        if (Result >= 0 && Result < 10){
+            printf(" -     %d\n", R3);
+            return 0;
+        }
     }
-    GetDigits(Num1, 0, 1);
-    GetDigits(Num2, 1, 1);
-    GetDigits(Ans, AType, 0);
-}
-
-int validate(int state, int num){
-    if (state == 0){
-        printf("Input must be an integer.\n");
-        return 1;
-    }
-    if (num > 999 || num < 100){
-        printf("Inputted integer is not a positive 3-digit integer.\n");
-        return 1;
-    }
+    if (Result >= 100){
+            printf("   %d %d %d\n", R1, R2, R3);
+            return 0;
+        }
+        if (Result >= 10 && Result < 100){
+            printf("     %d %d\n", R2, R3);
+            return 0;
+        }
+        if (Result >= 0 && Result < 10){
+            printf("       %d\n", R3);
+            return 0;
+        }
     return 0;
 }
 
-int ask(char question[]){
-    int input;
-    printf("%s", question);
-    int state = scanf("%d", &input);
-    if (validate(state, input) != 0) return -1;
-    return input;
+int ask(char Question[]){
+    int Tmp;
+    printf("%s", Question);
+    int state = scanf("%d", &Tmp);
+    if (state == 0){
+        printf("Unexpeted input.\n");
+        return -1;
+    }
+    if (Tmp > 999 && Tmp < 100){
+        printf("Inputted number is not a 3 digit non-negative integer.\n");
+        return -1;
+    }
+    return Tmp;
 }
 
 int main(){
-    int Num1 = ask("Please enter the first  number: ");
-    if (Num1 == -1){
-        printf("Error %d: Invalid Input!\n", GetErrorCode());
+    int First = ask("Please enter the first   number: ");
+    if (First == -1){
         return 1;
     }
-    int Num2 = ask("Please enter the second number: ");
-    if (Num2 == -1){
-        printf("Error %d: Invalid Input!\n", GetErrorCode());
+    int Second = ask("Please enter the second number: ");
+    if (Second == -1){
         return 1;
     }
-    Calculate(Num1, Num2);
+    int Result = First - Second;
+    FormatPrint(First, Second, Result);
     return 0;
 }
