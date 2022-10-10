d284973e813ef6db8bb8bf58be24252ba27af733
= <=>
2022-10-10 21:51:35 +0800

    removed unnecessary lines

--- Year1/Homework/HW1/hw0103.c
+++ Year1/Homework/HW1/hw0103.c
@@ -29,9 +29,6 @@ int HandleCalculations(int32_t A_X, int32_t A_Y, int32_t B_X, int32_t B_Y, int32
     float C_X_1 = C_X;
     float C_Y_1 = C_Y;
 
-    printf("%lf | %lf\n", A_X_1 - B_X_1, A_Y_1 - B_Y_1);
-    printf("DEBUG AX - BX and AY - BY != 0\n");
-
     if (A_X_1 - B_X_1 == 0 || A_Y_1 - B_Y_1 == 0){
         if (A_Y_1 - B_Y_1 == 0){
             printf("The line: x = %d\n", C_X);
@@ -42,12 +39,9 @@ int HandleCalculations(int32_t A_X, int32_t A_Y, int32_t B_X, int32_t B_Y, int32
     }
 
     double SlopeAB = GetSlope(A_X_1, A_Y_1, B_X_1, B_Y_1);
-    printf("SlopeAB = %lf\n", SlopeAB); //rmv later
     double SlopeCD = -1 / SlopeAB;
     double C = (SlopeCD * C_X_1 * -1) + C_Y_1;
-    printf("C is actually %.15f\n", C);
     if (C < 0){
-        printf("C < 0\n");
         printf("The line: y = %lf * x - %.6lf\n", SlopeCD, C * -1);
         return 0;
     }
