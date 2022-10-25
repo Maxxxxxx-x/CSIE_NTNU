82766f56bd0c817ef0c994273ab79ff28ed64899
= <=>
2022-10-25 19:06:13 +0800

    finished hw0201, updated hw0203

--- Year1/Homework/HW2/hw0201.c
+++ Year1/Homework/HW2/hw0201.c
@@ -1,53 +1,49 @@
 #include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 void Throw(char Error[], int Code){
     printf("%s\n", Error);
     exit(Code);
 }
 
-void CompareState(int State, int Required){
-    if (State == Required) return;
-    Throw("Invalid input.", 1);
+void Compare(int Num, int Required){
+    if (Num == Required) return;
+    Throw("Invalid input!", 1);
 }
 
-void CompareInt(uint16_t Val){
-    if (Val <= INT16_MAX) return;
-    Throw("Given integer is not 16 bits.", 1);
-}
-
-void Gregory(int i){
-    if (i % 2 == 0) i += 1;
-    double New = (double) i;
-    double Ans = 4 / New;
-    printf("    Gregory – Leibniz series: %f\n", Ans);
+void Handle(int Num){
+    double pi = 3.14159265358979323846;
+    double C_Pi, N_Pi;
+    int Count = 0;
+    for (int i = 1; i <= Num; i++){
+        printf("n = %d\n", (int)(i / 2) + 1);
+        if (i == 1){
+            C_Pi = 4;
+            N_Pi = 3;
+            printf("    Gregory – Leibniz series: %f (%lf)\n", C_Pi, fabs(pi - C_Pi));
+            printf("    Nilakanthaseries: %f (%lf)\n", N_Pi, fabs(pi - N_Pi));
+            continue;
+        }
+        if (i % 2 != 0){
+            C_Pi -= (4 / (double) i);
+            N_Pi += 4 / (double)((i - 1) * i * (i + 1));
+        }else{
+            C_Pi += 4 / (double) i;
+            N_Pi -= 4 / (double)((i - 1) * i * (i + 1) + 1);
+        }
+        printf("    Gregory – Leibniz series: %lf (%lf)\n", C_Pi, fabs(pi - C_Pi));
+        printf("    Nilakanthaseries: %lf (%lf)\n", N_Pi, fabs(pi - N_Pi));
+    }
     return;
-}   
-
+}
 
 int main(){
-    uint16_t Val;
-    printf("Please enter n (16-bits unsigned): ");
-    int State = scanf("%d", &Val);
-    CompareState(State, 1);
-    CompareInt(Val);
-    double N_Pi;
-    double N_S = 1;
-    for (int i = 1; i <= Val; i++){
-        printf("n = %d\n", i);
-        Gregory(i);
-        //Nilakantha
-        double N_Pi;
-        double N_S = 1;
-        for (double k = 2; k <= i * 2; k += 2){
-            N_Pi = N_Pi + N_S * (4 / (k * (k + 1) * (k + 2)));
-            N_S = -N_S;
-        }
-        double Between = 3.14159265358979323846 - N_Pi;
-        printf("    Nilakantha series: %f (%f)\n", N_Pi, Between);
-    }
+    uint16_t Num;
+    printf("Please enter n (16-bits unsigned):");
+    int state = scanf("%d", &Num);
+    Compare(state, 1);
+    Handle(Num);
     return 0;
 }
-
