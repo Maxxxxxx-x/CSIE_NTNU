b28322e78c37c5bfee82616d14eab451462c1f3d
= <=>
2022-10-25 14:41:56 +0800

    finished hw0203.c

--- Year1/Homework/HW2/hw0201.c
+++ Year1/Homework/HW2/hw0201.c
@@ -1,37 +1,53 @@
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdint.h>
 
+void Throw(char Error[], int Code){
+    printf("%s\n", Error);
+    exit(Code);
+}
+
+void CompareState(int State, int Required){
+    if (State == Required) return;
+    Throw("Invalid input.", 1);
+}
+
+void CompareInt(uint16_t Val){
+    if (Val <= INT16_MAX) return;
+    Throw("Given integer is not 16 bits.", 1);
+}
+
+void Gregory(int i){
+    if (i % 2 == 0) i += 1;
+    double New = (double) i;
+    double Ans = 4 / New;
+    printf("    Gregory – Leibniz series: %f\n", Ans);
+    return;
+}   
+
+
 int main(){
-    uint16_t n;
-    printf("Please enter  n (16-bits unsigned): ");
-    int state = scanf("%d", &n);
-    if (state != 1){
-        printf("Invalid input.");
-        return 1;
-    }
-
-    for (int i = 1; i <= n; i++){
+    uint16_t Val;
+    printf("Please enter n (16-bits unsigned): ");
+    int State = scanf("%d", &Val);
+    CompareState(State, 1);
+    CompareInt(Val);
+    double N_Pi;
+    double N_S = 1;
+    for (int i = 1; i <= Val; i++){
         printf("n = %d\n", i);
-        //Leibniz's Formula
-        int tmp1_L = 1;
-        int sum_L = 0;
-        for (int j = 0; j < n; j++){
-            if (j % 2 == 0){
-                sum_L += 4 / tmp1_L;
-            }else{
-                sum_L -= 4/tmp1_L;
-            }
-            tmp1_L += 2;
-            printf("Gregory – Leibniz series: %d (%lf)\n", sum_L, tmp1_L);
-            break;
+        Gregory(i);
+        //Nilakantha
+        double N_Pi;
+        double N_S = 1;
+        for (double k = 2; k <= i * 2; k += 2){
+            N_Pi = N_Pi + N_S * (4 / (k * (k + 1) * (k + 2)));
+            N_S = -N_S;
         }
-        //Nilakantha's Formula
-        int tmp1_N = 1;
-        int sum_N = 0;
-        for (int j = 0; j < n; j++){
-
-        }
+        double Between = 3.14159265358979323846 - N_Pi;
+        printf("    Nilakantha series: %f (%f)\n", N_Pi, Between);
     }
-
     return 0;
 }
+
---
+++ Year1/Homework/HW2/hw0203.c
@@ -0,0 +1,66 @@
+#include <math.h>
+#include <stdio.h>
+#include <limits.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+void Throw(char Error[], int Code){
+    printf("%s\n", Error);
+    exit(Code);
+}
+
+void Compare(int Num, int Required){
+    if (Num == Required) return;
+    Throw("Invalid input!", 1);
+}
+
+int GetDigits(int64_t Num){
+    return Num % 10;
+}
+
+void Calculate(uint Num){
+    int length =  (int)(log10(Num) + 1);
+    if (length < 0) Throw("Unexpected error: Array length is < 0", 1);
+    int Digits[(int)(log10(Num) + 1)];
+    int Count = 0;
+    int Sum = 0;
+    if (Num < 10){
+        printf("Final: %d\n", Num);
+        return;
+    }
+
+    while (Num > 0){
+        int Digit = GetDigits(Num);
+        Digits[Count] = Digit;
+        Num /= 10;
+        Count += 1;
+    }
+
+    for (int i = Count - 1; i >= 0; i--){
+        Sum += Digits[i];
+        if (i != 0 ){
+            printf("%d + ", Digits[i]);
+            continue;
+        }
+        printf("%d = ", Digits[i]);
+    }
+    printf("%d\n", Sum);
+    if (Sum >= 10) Calculate(Sum);
+    return;
+}
+
+
+int main(){
+    uint32_t Num;
+    printf("Please enter a number: ");
+    int State = scanf("%d", &Num);
+    Compare(State, 1);
+    if (Num > INT32_MAX){
+        printf("Overflow occured!!!\n");
+        uint New = 2147483647 + (2147483649 + Num);
+        Calculate(New);
+        return 0;
+    }
+    Calculate(Num);
+    return 0;
+}
